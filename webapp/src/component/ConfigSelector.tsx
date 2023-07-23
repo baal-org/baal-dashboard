f8a6818ea9d5b81906be57b0ff3757d4f2016f55
Frédéric Branchaud-Charron <frederic.branchaud.charron@gmail.com>
2023-07-23 12:37:39 -0400

    Improve style of config selector (#13)

--- webapp/src/component/ConfigSelector.tsx
+++ webapp/src/component/ConfigSelector.tsx
@@ -1,7 +1,17 @@
-import React, { useEffect, useState } from "react";
-import Select from "react-select";
+import ErrorIcon from "@mui/icons-material/Error";
+import {
+  Box,
+  CircularProgress,
+  Typography,
+  styled,
+  useTheme,
+} from "@mui/material";
 import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
+import Select from "react-select";
 import ExperimentInterface from "../interfaces/ExperimentInterface";
+import { tokens } from "../theme";
 
 /**
  * ConfigSelector is a functional component that renders a select dropdown component and allows the user
@@ -22,19 +32,39 @@ import ExperimentInterface from "../interfaces/ExperimentInterface";
  * @see ExperimentInterface
  */
 
+const ErrorBox = styled(Box)({
+  margin: "10px",
+  display: "flex",
+});
+
 function ConfigSelector({ onSelect }: { onSelect: any }) {
-  let [experiments, setExperiments] = useState<ExperimentInterface[]>([]);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios("/experiments");
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: [],
+    queryFn: () => axios.get("/experiments").then((res) => res.data),
+    enabled: true,
+  });
 
-      setExperiments(result.data);
-    };
+  if (isLoading) {
+    return (
+      <Box>
+        <CircularProgress color="secondary" />
+      </Box>
+    );
+  }
 
-    fetchData();
-  }, []);
+  if (isError || data === undefined) {
+    return (
+      <ErrorBox>
+        <ErrorIcon sx={{ marginRight: "5px" }} />
+        <Typography>Can't access experiments</Typography>
+      </ErrorBox>
+    );
+  }
 
+  const experiments: ExperimentInterface[] = data;
   const options = experiments.map((item) => {
     return {
       value: item.exp_id,
@@ -51,7 +81,36 @@ function ConfigSelector({ onSelect }: { onSelect: any }) {
     };
   });
 
-  return <Select options={options} onChange={onSelect} />;
+  return (
+    <Select
+      options={options}
+      onChange={onSelect}
+      theme={(theme) => ({
+        ...theme,
+        borderRadius: 0,
+        colors: {
+          ...theme.colors,
+          primary: colors.primary[800],
+          primary75: colors.primary[600],
+          primary50: colors.primary[500],
+          primary25: colors.primary[400],
+          neutral0: colors.primary[200],
+          neutral5: colors.grey[200],
+          neutral10: colors.grey[300],
+          neutral20: colors.grey[400],
+          neutral30: colors.grey[500],
+          neutral40: colors.grey[600],
+          neutral50: colors.grey[700],
+          neutral60: colors.grey[800],
+          neutral70: colors.grey[900],
+          neutral80: colors.grey[900],
+          neutral90: colors.grey[900],
+          danger: colors.greenAccent[500],
+          dangerLight: colors.greenAccent[500],
+        },
+      })}
+    />
+  );
 }
 
 export default ConfigSelector;
